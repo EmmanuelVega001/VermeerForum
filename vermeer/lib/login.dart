f4e9806e320b3f9103968a635bcd9c1f734bf2f0
EmmanuelVega001 <72900109+EmmanuelVega001@users.noreply.github.com>
2023-07-31 07:11:51 -0600

    recuperada del login jeje

--- vermeer/lib/login.dart
+++ vermeer/lib/login.dart
@@ -9,7 +9,61 @@ class login extends StatefulWidget {
 
 class _loginState extends State<login> {
   @override
-  Widget build(BuildContext context) {
-    return const Placeholder();
+Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: Text("Login Page"),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(top: 60.0),
+              child: Center(
+                child: Container(
+                    width: 200,
+                    height: 150,
+                    /*decoration: BoxDecoration(
+                        color: Colors.red,
+                        borderRadius: BorderRadius.circular(50.0)),*/
+                    child: const Text("VERMEER FORUM",style: TextStyle(fontSize: 30, color:Color.fromARGB(255, 192, 63, 63)),)),
+              ),
+            ),
+            Padding(
+              //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
+              padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'USER',
+                    hintText: 'ENTER A VALID USERNAME PLEASE'),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(
+                  left: 15.0, right: 15.0, top: 15, bottom: 0),
+              //padding: EdgeInsets.symmetric(horizontal: 15),
+              child: TextField(
+
+                obscureText: true,
+                decoration: InputDecoration(
+                    border: OutlineInputBorder(),
+                    labelText: 'Password',
+                    hintText: 'Enter secure password'),
+              ),
+            ),
+           
+            Container(
+
+            ),
+            SizedBox(
+              height: 130,
+            ),
+            Text('New User? Create Account')
+          ],
+        ),
+      ),
+    );
   }
 }
